0bf2acff577d7bce732c6d80d9e01cd03e523bcd
bukhaled2002 <bakhaled.310@gmail.com>
2023-11-01 16:23:29 -0700

    f

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL) return NULL;
 	new_node->parent = parent;
-	new_node->value = value;
+	new_node->n = value;
 	new_node->left = new_node->right = NULL;
 	return (new_node);
 
