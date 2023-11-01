0849cf52eea8e656c976ac5f8510490f6425d84f
bukhaled2002 <bakhaled.310@gmail.com>
2023-11-01 16:29:09 -0700

    f

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_insert_left - creates a binary tree node
+ * @parent: parent of the node to create
+ * @value: value to store in new node
+ *
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left_child;
+
+	if (parent == NULL)
+		return (NULL);
+	left_child = binary_tree_node(parent, value);
+	if (left_child == NULL)
+		return (NULL);
+	left_child->left = parent->left;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+	parent->left = left_child;
+	return (left_child);
+}
