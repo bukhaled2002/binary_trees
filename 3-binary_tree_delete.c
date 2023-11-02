256ef3ef16f3502aefe210318087ad2f5ca115dd
bukhaled2002 <bakhaled.310@gmail.com>
2023-11-02 14:25:33 -0700

    fg

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 
 /**
- * binary_tree_insert_right - creates a binary tree node
- * @parent: parent of the node to create
- * @value: value to store in new node
+ * binary_tree_delete - creates a binary tree node
+ * @tree: parent of the node to create
  *
- * Return: pointer to the new node
+ * Return:
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	
+
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
